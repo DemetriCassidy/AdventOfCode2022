327985dc76313bbc198673cd20d848395f3cddcd
Demetri Cassidy <demetricassidy99@gmail.com>
2023-02-21 18:06:21 -0500

    implemented day 6 solution

---
+++ Day 6/day_6_part_1.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	// set up file scanner
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	// obtain line of input
+	scanner.Scan()
+	buffer := scanner.Text()
+
+	// determine where marker is
+	numCharsNeeded := 4
+	for i := range buffer {
+		initialChars := make(map[rune]bool)
+		for j := 0; j < numCharsNeeded; j++ {
+			char := rune(buffer[i+j])
+			initialChars[char] = true
+		}
+		if len(initialChars) == numCharsNeeded {
+			fmt.Println(i + numCharsNeeded)
+			break
+		}
+	}
+}
