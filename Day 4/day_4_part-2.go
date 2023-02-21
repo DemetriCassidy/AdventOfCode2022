6fbec344c4ec4cc48c8495ed54af0d1337d19c8a
Demetri Cassidy <demetricassidy99@gmail.com>
2023-02-20 22:36:29 -0500

    implemented day 4 solution

---
+++ Day 4/day_4_part-2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	// set up file scanner
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	// parse pairs of ranges and determine if one is fully contained by the other
+	// if so, increment numOverlappingRanges
+	numOverlappingRanges := 0
+
+	for scanner.Scan() {
+		var elfOneStart, elfOneEnd, elfTwoStart, elfTwoEnd int
+
+		_, err := fmt.Sscanf(scanner.Text(), "%d-%d,%d-%d", &elfOneStart, &elfOneEnd, &elfTwoStart, &elfTwoEnd)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if elfTwoStart <= elfOneEnd && elfTwoEnd >= elfOneStart || elfOneStart <= elfTwoEnd && elfOneEnd >= elfTwoStart {
+			numOverlappingRanges += 1
+		}
+	}
+
+	// print numFullyContainedRanges
+	fmt.Printf("# of fully contained ranges: %d\n", numOverlappingRanges)
+}
